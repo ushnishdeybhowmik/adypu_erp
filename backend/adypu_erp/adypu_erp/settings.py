fbbd969456415f47fc603e9f40ca093d92395aed
ushnishdeybhowmik <neilbhowmik05@gmail.com>
2023-10-01 23:58:23 +0530

    #5 Student App - Updated Model

--- backend/adypu_erp/adypu_erp/settings.py
+++ backend/adypu_erp/adypu_erp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'adypu_student_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'adypu_erp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': '',
+        'PORT': ''
     }
 }
 
--- backend/adypu_erp/adypu_student_app/models.py
+++ backend/adypu_erp/adypu_student_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class StudentData(models.Model):
+    URN = models.CharField(max_length=17, primary_key=True)
+    First_Name = models.CharField(max_length=45)
+    Middle_Name = models.CharField(max_length=45, null=True)
+    Last_Name = models.CharField(max_length=45)
+    Mother_name = models.CharField(max_length=45, null=True)
+    Gender = models.CharField(max_length=45)
+    DOB = models.DateField()
+    Local_Address = models.CharField(max_length=225)
+    Permanent_Address = models.CharField(max_length=225)
+    Email = models.CharField(max_length=30)
+    Student_Phone_No = models.IntegerField()
+    Emergency_Phone = models.IntegerField()
