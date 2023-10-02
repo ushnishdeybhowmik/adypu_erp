1e75b826eb953258ae2b91cac19d4c706ca9c3c5
ushnishdeybhowmik <neilbhowmik05@gmail.com>
2023-10-02 10:31:31 +0530

    #6 Student App - Model Updated. Replaced Enum

--- backend/adypu_erp/adypu_student_app/models.py
+++ backend/adypu_erp/adypu_student_app/models.py
@@ -1,6 +1,53 @@
 from django.db import models
 
 class StudentData(models.Model):
+    
+    PHYSICAL_HANDICAP_CHOICES = [
+        ("Y", "YES"),
+        ("N", "NO")
+    ]
+    
+    CATEGORY_CHOICES = [
+        ("GEN", "GENERAL"),
+        ("OBC", "OBC"),
+        ("SC", "SC"),
+        ("ST", "ST"),
+        ("EWS", "EWS")
+    ]
+    
+    BLOOD_GROUP_CHOICES = [
+        ("O+", "O POSITIVE"),
+        ("O-", "O NEGATIVE"),
+        ("A+", "A POSITIVE"),
+        ("A-", "A NEGATIVE"),
+        ("B+", "B POSITIVE"),
+        ("B-", "B NEGATIVE"),
+        ("AB+", "AB POSITIVE"),
+        ("AB-", "AB NEGATIVE"),
+        ("BOM", "BOMBAY")
+    ]
+    
+    RELIGION_CHOICES = [
+        ("HI", "HINDU"),
+        ("MU", "MUSLIM"),
+        ("CH", "CHRISTIAN"),
+        ("SI", "SIKH"),
+        ("PA", "PARSI"),
+        ("JE", "JEWISH"),
+        ("BU", "BUDDHIST"),
+        ("TA", "TAOIST"),
+        ("OT", "OTHER")
+    ]
+    
+    STUDENT_STATUS_CHOICES = [
+        ("Y", "ACTIVE"),
+        ("N", "INACTIVE")
+    ]
+    
+    EDUATION_LOAN_CHOICES = [
+        ("Y", "YES"),
+        ("N", "NO")
+    ]
     URN = models.CharField(max_length=17, primary_key=True)
     First_Name = models.CharField(max_length=45)
     Middle_Name = models.CharField(max_length=45, null=True)
@@ -13,3 +60,5 @@ class StudentData(models.Model):
     Email = models.CharField(max_length=30)
     Student_Phone_No = models.IntegerField()
     Emergency_Phone = models.IntegerField()
+    Aadhar_Card_No = models.IntegerField()
+    Physical_Handicap = models.e
